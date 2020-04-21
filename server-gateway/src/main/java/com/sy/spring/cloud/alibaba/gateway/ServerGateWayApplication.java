034d33eada576ffe315d0e74bd55e5acc9c8da71
shenyao2333 <shenyao2333@163.com>
2020-04-21 22:08:05 +0800

    1、网关模块去除认证

--- server-gateway/src/main/java/com/sy/spring/cloud/alibaba/gateway/ServerGateWayApplication.java
+++ server-gateway/src/main/java/com/sy/spring/cloud/alibaba/gateway/ServerGateWayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 
 /**
---
+++ server-gateway/src/main/java/com/sy/spring/cloud/alibaba/gateway/config/SwaggerConfig.java
@@ -0,0 +1,76 @@
+
+package com.sy.spring.cloud.alibaba.gateway.config;
+
+import org.assertj.core.util.Lists;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+
+/**
+ * @author sy
+ * @date Created in 2020.4.6 16:10
+ * @description
+ */
+@Component
+@EnableSwagger2
+@Primary
+public class SwaggerConfig implements  SwaggerResourcesProvider {
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Java服务端API列表")
+                .description("邮箱：sy91aa@163.com")
+                .termsOfServiceUrl("")
+                .contact(new Contact("sy", "", "sy91aa@163.com"))
+                .version("1.0")
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration(null, "list", "alpha", "schema", UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS,
+                false, true, 60000L);
+    }
+
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = Lists.newArrayList();
+        resources.add(swaggerResource("用户模块", "/auth-user/v2/api-docs", "1.0"));
+        resources.add(swaggerResource("业务服务", "/api-tripartite/v2/api-docs", "1.0"));
+        resources.add(swaggerResource("微信服务", "/api-wechat/v2/api-docs", "1.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
+
+
+
+}
+
