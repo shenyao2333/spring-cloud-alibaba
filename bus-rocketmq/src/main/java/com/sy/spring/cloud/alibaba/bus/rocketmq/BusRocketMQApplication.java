847b70821e4ac367e4e56a1f591048624b942c29
shenyao <sy91aa@163.com>
2020-06-02 20:40:48 +0800

    1、RocketMQ模块

---
+++ bus-rocketmq/src/main/java/com/sy/spring/cloud/alibaba/bus/rocketmq/BusRocketMQApplication.java
@@ -0,0 +1,29 @@
+package com.sy.spring.cloud.alibaba.bus.rocketmq;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author sy
+ * @date Created in 2020.4.26 15:03
+ * @description 启动类
+ */
+@EnableSwagger2
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAsync
+@MapperScan(basePackages = "com.sy.spring.cloud.alibaba.business.social.mapper")
+@ComponentScan(basePackages = {"com.sy.spring.cloud.alibaba.bus.rocketmq", "com.sy.spring.cloud.alibaba.module" })
+@EnableCaching
+public class BusRocketMQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusRocketMQApplication.class, args);
+    }
+}
