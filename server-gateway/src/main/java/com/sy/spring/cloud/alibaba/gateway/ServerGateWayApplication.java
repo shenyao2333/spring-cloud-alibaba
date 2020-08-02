ca3da42973aa7167332e289006a4e1fbc73f03ed
shenyao2333 <shenyao2333@163.com>
2020-08-02 18:15:30 +0800

    1、正在接入限流

--- server-gateway/src/main/java/com/sy/spring/cloud/alibaba/gateway/ServerGateWayApplication.java
+++ server-gateway/src/main/java/com/sy/spring/cloud/alibaba/gateway/ServerGateWayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
---
+++ server-gateway/src/main/java/com/sy/spring/cloud/alibaba/gateway/config/RequestRateLimiterConfig.java
@@ -0,0 +1,12 @@
+package com.sy.spring.cloud.alibaba.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author sy
+ * @date Created in 2020.8.2 18:14
+ * @description 限流的配置类
+ */
+@Configuration
+public class RequestRateLimiterConfig {
+}
