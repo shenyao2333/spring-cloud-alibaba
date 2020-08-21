4c454c8f5225dda7ab4970c5dc5b101edb6075e3
shenyao2333 <shenyao2333@163.com>
2020-08-21 22:27:14 +0800

    es项目配置

--- paascloud-generator/paascloud-generator-es/src/main/java/com/sy/spring/cloud/alibaba/generator/es/GeneratorEsApplication.java
+++ paascloud-generator/paascloud-generator-es/src/main/java/com/sy/spring/cloud/alibaba/generator/es/GeneratorEsApplication.java
@@ -1,10 +1,14 @@
 package com.sy.spring.cloud.alibaba.generator.es;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author sy
@@ -15,6 +19,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCaching
+@EnableSwagger2
+@MapperScan(basePackages = "com.sy.spring.cloud.alibaba.auth.user.mapper")
+@ComponentScan(basePackages = {"com.sy.spring.cloud.alibaba.provider.basic"})
+@EnableFeignClients("com.sy.spring.cloud.alibaba.provider.api.feign")
 public class GeneratorEsApplication {
 
     public static void main(String[] args) {
